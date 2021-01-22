d00a06973d0697ed2f32d64823668110037a0c7e
DonaldReac <40219825+DonaldReac@users.noreply.github.com>
2021-01-22 16:05:55 -0600

    fuentes

--- build-RestAu-Desktop_Qt_5_12_8_MinGW_32_bit-Debug/ui_iniciosesion.h
+++ build-RestAu-Desktop_Qt_5_12_8_MinGW_32_bit-Debug/ui_iniciosesion.h
@@ -11,6 +11,7 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
@@ -22,6 +23,9 @@ class Ui_InicioSesion
 {
 public:
     QWidget *centralwidget;
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -32,9 +36,31 @@ public:
         InicioSesion->resize(800, 600);
         centralwidget = new QWidget(InicioSesion);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(290, 30, 141, 91));
+        QFont font;
+        font.setFamily(QString::fromUtf8("Magenta latte"));
+        font.setPointSize(48);
+        label->setFont(font);
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(230, 110, 301, 71));
+        QFont font1;
+        font1.setFamily(QString::fromUtf8("Flow Ext"));
+        font1.setPointSize(26);
+        label_2->setFont(font1);
+        label_3 = new QLabel(centralwidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setGeometry(QRect(200, 170, 361, 81));
+        QFont font2;
+        font2.setFamily(QString::fromUtf8("Flow Ext"));
+        font2.setPointSize(12);
+        label_3->setFont(font2);
         InicioSesion->setCentralWidget(centralwidget);
         menubar = new QMenuBar(InicioSesion);
         menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 21));
         InicioSesion->setMenuBar(menubar);
         statusbar = new QStatusBar(InicioSesion);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -48,6 +74,11 @@ public:
     void retranslateUi(QMainWindow *InicioSesion)
     {
         InicioSesion->setWindowTitle(QApplication::translate("InicioSesion", "InicioSesion", nullptr));
+        label->setText(QApplication::translate("InicioSesion", "RESTAU", nullptr));
+        label_2->setText(QApplication::translate("InicioSesion", "Pizza            $200", nullptr));
+        label_3->setText(QApplication::translate("InicioSesion", "deliciosa pizza con queso derretido de la mejor calidad\n"
+" con los mejores ingredientes y con orilla rellena\n"
+"de queso ", nullptr));
     } // retranslateUi
 
 };
